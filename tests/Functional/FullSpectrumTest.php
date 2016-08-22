efe1ebd467e21219b44f8b2db403f0fc86a5e7ae
Marcel Hernandez <e6990620@gmail.com>
2016-08-22 16:51:38 +0200

    and now we test

---
+++ tests/Functional/FullSpectrumTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace UMA\Tests\SchemaBundle;
+
+use Symfony\Component\BrowserKit\Client;
+
+class FullSpectrumTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Client
+     */
+    private $client;
+
+    protected function setUp()
+    {
+        $kernel = new \TestKernel('test', true);
+        $kernel->boot();
+
+        $this->client = $kernel->getContainer()->get('test.client');
+
+        parent::setUp();
+    }
+
+    /**
+     * @test
+     */
+    public function validRequest()
+    {
+        $this->makeAndCheckRequest(200, '{"name": "John Doe", "age": 30}');
+    }
+
+    /**
+     * @test
+     * @dataProvider invalidRequestsProvider
+     *
+     * @param string $invalidPayload
+     */
+    public function invalidRequests($invalidPayload)
+    {
+        $this->makeAndCheckRequest(400, $invalidPayload);
+    }
+
+    public function invalidRequestsProvider()
+    {
+        return [
+            'empty body' => [''],
+            'no JSON' => ['huehue'],
+            'empty JSON object' => ['{}'],
+            'missing field' => ['{"name": "John Doe"}'],
+            'invalid field type' => ['{"name": null, "age": 30}'],
+            'invalid field value' => ['{"name": "Old Doe", "age": 99}']
+        ];
+    }
+
+    private function makeAndCheckRequest($expectedStatusCode, $payload)
+    {
+        $this->client
+            ->request('GET', '/', [], [], [], $payload);
+
+        $this->assertSame($expectedStatusCode, $this->client->getInternalResponse()->getStatus());
+    }
+}
---
+++ tests/TestProject/src/AppBundle/Controller/TestController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace TestProject\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use UMA\SchemaBundle\Annotation\JsonSchema;
+
+class TestController extends Controller
+{
+    /**
+     * @JsonSchema(uri="person.json")
+     */
+    public function indexAction(Request $request)
+    {
+        $receivedData = json_decode($request->getContent(), true);
+
+        if (
+            isset($receivedData['age']) &&
+            isset($receivedData['name']) &&
+            is_int($receivedData['age']) &&
+            is_string($receivedData['name'])
+        ) {
+            return new Response(
+                'Lookin\' good',
+                Response::HTTP_OK
+            );
+        } else {
+            return new Response(
+                'With the @JsonSchema annotation invalid data should never reach the controller',
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+}
